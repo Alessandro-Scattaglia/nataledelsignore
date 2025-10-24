a3ea763f7e18e52358a900473e7530b3f1d9c689
Alessandro-Scattaglia <alessandro.scattaglia@istitutoagnelli.it>
2025-10-24 17:00:22 +0200

    fix paths for GitHub Pages

--- docs/_app/immutable/chunks/BPS81YAf.js
+++
@@ -1,1 +0,0 @@
-const s=globalThis.__sveltekit_n05lqx?.base??"/nataledelsignore",e=globalThis.__sveltekit_n05lqx?.assets??s??"";export{e as a,s as b};
---
+++ docs/_app/immutable/chunks/C69iEoFe.js
@@ -0,0 +1,1 @@
+const s=globalThis.__sveltekit_bhiveg?.base??"/nataledelsignore",e=globalThis.__sveltekit_bhiveg?.assets??s??"";export{e as a,s as b};
