10a63b842a5d9f8278daa5d945d5fc1c508073e1
Alessandro-Scattaglia <alessandro.scattaglia@istitutoagnelli.it>
2025-10-24 16:56:24 +0200

    rebuild docs with correct paths

---
+++ docs/_app/immutable/chunks/BPS81YAf.js
@@ -0,0 +1,1 @@
+const s=globalThis.__sveltekit_n05lqx?.base??"/nataledelsignore",e=globalThis.__sveltekit_n05lqx?.assets??s??"";export{e as a,s as b};
--- docs/_app/immutable/chunks/DRgmiS8e.js
+++
@@ -1,1 +0,0 @@
-const s=globalThis.__sveltekit_1yvtvao?.base??"/nataledelsignore",a=globalThis.__sveltekit_1yvtvao?.assets??s??"";export{a,s as b};
